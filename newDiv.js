08c97da4c9595bf7e1fde3f31cd75235714d35f8
Konstantin <konstantin.v.modin@gmail.com>
2018-12-28 08:27:40 +0100

    newDiv

---
+++ newDiv.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+class Options {
+    constructor(height, width, bg, fontSize, textAlign) {
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+        this.margin = 'auto';
+        // this.i = 1;
+    }
+    newDiv() {
+        var div = document.createElement('div');
+        document.body.appendChild(div);
+        div.textContent = prompt('Введите текст', 'New div');
+        // console.log(this.i);
+        var s = div.style;
+        s.height = this.height;
+        s.width = this.width;
+        s.background = this.bg;
+        s.fontSize = this.fontSize;
+        s.textAlign = this.textAlign;
+        s.margin = this.margin;
+    }
+}
+
+var optionB = new Options('100px', '300px', 'red', '150%', 'center');
+console.log(optionB);
+optionB.newDiv();
